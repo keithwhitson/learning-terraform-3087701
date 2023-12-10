1aaf0c23ba19ac1963d605db0c6e2c19f24b9233
Keith Whitson <keith_whitson@yahoo.com>
2023-12-10 17:48:22 +0100

    Update main.tf
    changed the subnet id to one that exists

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
-
+ subnet_id     = "subnet-0c3840e2c57a8b7c6"
   tags = {
     Name = "HelloWorld"
   }
