d231ddbd8ed7fd0e328ccfff7b47cf550adc0c95
Keith Whitson <keith_whitson@yahoo.com>
2023-12-11 16:36:29 +0100

    comment out instance

--- main.tf
+++ main.tf
@@ -47,6 +47,25 @@ vpc_security_group_ids = [module.blog_sg.security_group_id]
   }
 }
 
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.3.1"
+
+  name = "blog"
+  min_size = 1
+  max_size = 2
+
+ vpc_zone_identifier = module.blog_vpc.public_subnets
+ target_group_arns   = module.blog_alb.target_group_arns
+
+  security_groups  = [module.blog_sg.security_group_id]
+  
+  ami            = data.aws_ami.app_ami.id
+  instance_type  = var.instance_type
+
+
+}
+
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
